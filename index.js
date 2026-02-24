1948743c198fe0fa974d79f3f7b92a1e0fa79f73
abdusattarov3105-max <abdusattarov3105@gmail.com>
2026-02-24 21:32:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-index.js
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, "Bot ishlayapti 🚀");
+});
