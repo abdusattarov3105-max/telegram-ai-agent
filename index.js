64f102e85fec69a3de0e15ad03655a755b01f3a3
abdusattarov3105-max <abdusattarov3105@gmail.com>
2026-02-24 21:45:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-import TelegramBot from 'node-telegram-bot-api';
+import express from "express";
+import TelegramBot from "node-telegram-bot-api";
+
+const app = express();
+app.use(express.json());
 
 const token = process.env.TELEGRAM_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, "Bot ishlayapti 🚀");
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("Bot ishlayapti 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server started");
 });
